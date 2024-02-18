830071d6ad2ce4b24061516637ebde0313f08638
takey <takehisa.y@gmail.com>
2024-02-19 06:01:28 +0900

    cors

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  // baseURL: "http://localhost:5000/api",
-  baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
+  baseURL: "http://localhost:5000/api",
+  //baseURL: process.env.NEXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
